2b11c88b74acffb6898b0c131f448948788eb823
gameslive360-oss <gameslive360@gmail.com>
2025-12-18 19:46:07 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,10 @@
 // Cole suas chaves do Firebase aqui
 export const firebaseConfig = {
-    apiKey: "SUA_API_KEY_AQUI",
-    authDomain: "seu-projeto.firebaseapp.com",
-    projectId: "seu-projeto",
-    storageBucket: "seu-projeto.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef"
+    apiKey: "AIzaSyBM9Nhu8KMNyF5xeNZzKgCLlzxDTf5g07I",
+    authDomain: "firecheckpro-4015c.firebaseapp.com",
+    projectId: "firecheckpro-4015c",
+    storageBucket: "firecheckpro-4015c.firebasestorage.app",
+    messagingSenderId: "510369151992",
+    appId: "1:510369151992:web:d148bd437ae55673ed740d",
+    measurementId: "G-RFP8P7JDEZ"
 };

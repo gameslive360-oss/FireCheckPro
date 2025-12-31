07edff248a4ab3151ceccb87c21bebe53a60887b
gameslive360-oss <gameslive360@gmail.com>
2025-12-31 18:50:24 -0300

    Fix - Create paste public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./js/**/*.js"
+    "./public/index.html",    
+    "./public/js/**/*.js"
   ],
   theme: {
     extend: {},

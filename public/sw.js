d6fce4c393a8bab93ce9180f1901a57b6459d5d4
gameslive360-oss <gameslive360@gmail.com>
2026-01-18 13:51:55 -0300

    Correções - Correções nos textos abreviados e place holdes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // MUDE AQUI: Suba a versão para forçar a atualização
-const CACHE_NAME = 'firecheck-v11'; // Mudei para v5
+const CACHE_NAME = 'firecheck-v13'; // Mudei para v5
 
 const urlsToCache = [
   './',
